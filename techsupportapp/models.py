034bcef17753df828c22ff6ff66fd8bedcdd6fcb
Yurij M <Yurij2015@users.noreply.github.com>
2021-01-03 16:29:23 +0300

    add models

--- techsupportapp/models.py
+++ techsupportapp/models.py
@@ -3,14 +3,30 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question = models.CharField(max_length=300)
-    answer = models.CharField(max_length=300)
-    status = models.CharField(max_length=300)
+    question = models.TextField()
+    answer = models.TextField()
+    answerDate = models.DateField()
+    status = models.IntegerField()
     user = models.IntegerField()
-    category = models.IntegerField()
     division = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
 
 
+class Employee(models.Model):
+    name = models.CharField(max_length=40)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Division(models.Model):
+    name = models.CharField(max_length=40)
 
