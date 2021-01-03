a0f89e22dce932885fcc59bb674b7538bb4af17c
Yurij M <Yurij2015@users.noreply.github.com>
2021-01-03 23:02:16 +0300

    add admin template

--- techsupportapp/apps.py
+++ techsupportapp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TechsupportappConfig(AppConfig):
     name = 'techsupportapp'
-    verbose_name = 'тех сопровождение'
+    verbose_name = 'Техническая поддержка'
